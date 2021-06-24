04dba67cdece164b74e34c3736c7b5cc3cea6294
paulaLopes2270 <plnhschltz@gmail.com>
2021-06-23 22:06:17 -0300

    älteração css botão e caixa

--- src/styles/buttons-styles.js
+++ src/styles/buttons-styles.js
@@ -1,28 +1,29 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Botao = styled.button`
-    border-radius: 10px;
-    border: none;
-    margin:10px;
-    padding:8px 80px;
-    font-size: 18px;
-    font-weight: 450;
-    cursor: pointer;
-    background-color: #481ed6;
-    color:#fff;
-    transition: 0.4s;
+  border-radius: 10px;
+  border: none;
+  margin: 10px;
+  width: 180px;
+  padding: 6px;
+  text-align: center;
+  justify-content: center;
+  font-size: 18px;
+  font-weight: 450;
+  cursor: pointer;
+  background-color: ${({ background }) => background || "#481ed6"};
+  color: #fff;
+  transition: 0.4s;
 
-    &:hover{
-        filter: brightness(0.8); 
-       
-    }
-
+  &:hover {
+    filter: brightness(0.8);
+  }
 `;
 
 export const BotaoSecundario = styled(Botao)`
-    background-color:#fff;
-    a {
-        text-decoration: none;
-        color:#481ed6;
-    }
+  background-color: #fff;
+  a {
+    text-decoration: none;
+    color: #481ed6;
+  }
 `;
--- src/styles/geral-styles.js
+++ src/styles/geral-styles.js
@@ -6,18 +6,19 @@ export const DivGrupo = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   gap: 10px;
-  width: 30%;
+  width: 90%;
+  max-width: 400px;
   margin: 4% auto;
   padding: 15px;
   border: solid 2px #481ed6;
   box-shadow: 0 0 10px 0 rgb(50 50 50 / 82%);
-  
-  h1{
-    color:#481ed6;
+
+  h1 {
+    color: #481ed6;
     margin-bottom: 10px;
   }
 
-  label{
+  label {
     font-size: 14px;
   }
 
@@ -34,23 +35,22 @@ export const DivGrupo = styled.div`
       overflow: hidden;
       border-radius: 3px;
 
-      &:focus{
-      border-color: #481ed6;
-      box-shadow: 0 0 10px 0 rgb(50 50 50 / 82%);
+      &:focus {
+        border-color: #481ed6;
+        box-shadow: 0 0 10px 0 rgb(50 50 50 / 82%);
+      }
     }
-  }
-
   }
   .senha,
   .cep {
     flex: 1 1 40%;
   }
 
-  .cpf{
+  .cpf {
     flex: 1 1 40%;
   }
   .username,
-  .nascimento{
+  .nascimento {
     flex: 1 1 40%;
   }
   .telefone,
@@ -65,20 +65,17 @@ export const DivGrupo = styled.div`
   .butons {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-    p{
+    p {
       text-align: center;
       padding-top: 10px;
     }
 
-    a{
+    a {
       text-decoration: none;
-      color:#481ed6;
-      font-weight: 400;
-      margin: 15px;
-      display: flex;
-      justify-content: end;
+      color: #481ed6;
     }
   }
 `;
-
