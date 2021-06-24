8c8e8c6b032cb86f5002714aac7291fbdac0e469
alec2 <aleccanto@gmail.com>
2021-06-24 20:03:20 -0300

    criacao da api pedido

---
+++ src/pages/detalhesProduto/index2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const DetalhesProduto = () => (
+    <div>
+        <Header/>
+        <h1>Carrinho</h1>
+    </div>
+)
+
+export default DetalhesProduto
